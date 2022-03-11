e00ce41ec8828d0e1bf6663cbcdc15613dd813d7
Ariana Tran <atran2@mail.umw.edu>
2022-03-11 13:23:10 -0500

    change to admin heading

--- spcarepo/login_form.php
+++ spcarepo/login_form.php
@@ -24,7 +24,10 @@
         echo "<script type=\"text/javascript\">window.location = \"index.php\";</script>";
     }
     if (!array_key_exists('_submit_check', $_POST)) {
-        echo('<div align="left"><p>Access to Homebase requires a Username and a Password. ' .
+        /*echo('<div align="left"><p>Access to Homebase requires a Username and a Password. ' .
+        '<ul>'
+        );*/
+        echo('<div align="left"><h1>Administrative Login' .
         '<ul>'
         );
         echo('<li>If you are applying for a volunteer position, enter the Username \'guest\' and a blank Password. ');

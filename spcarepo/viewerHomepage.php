1a6515e2e8cf6bfa0586b8221c1df09e7d37f8eb
Ariana Tran <atran2@mail.umw.edu>
2022-03-14 00:36:34 -0400

    made a rough draft of the viewer homepage. Very ugly

--- spcarepo/viewerHomepage.php
+++ spcarepo/viewerHomepage.php
@@ -1,3 +1,9 @@
-<?php
-echo 'Welcome to the SPCA Higlights';
-?>
+<html>
+<body>
+<img src="spca.jpg" alt="SPCA Logo">
+<h1>SPCA Adoption Story Highlights</h1>
+<form action="login_form.php" method="get">
+<input type="submit" value="Admin Login">
+</form>
+</body>
+</html>

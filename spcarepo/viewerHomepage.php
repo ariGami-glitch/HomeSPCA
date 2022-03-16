3b213f4406ea9f41f407b71f23cdb7fc92518659
Ariana Tran <atran2@mail.umw.edu>
2022-03-15 22:21:29 -0400

    playing around

--- spcarepo/viewerHomepage.php
+++ spcarepo/viewerHomepage.php
@@ -1,4 +1,4 @@
 <?php
-echo 'SPCA Story Highlights';
+echo '<h1>SPCA Story Highlights';
 echo('<p><form method="post"><input type="hidden" name="clicked" value="true"><input type="submit" name="adminlog" value="Administrative Login">');
 ?>

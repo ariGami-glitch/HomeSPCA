e0a7380c0cc4becfef9ab2b628d2f04db1f02692
Brian <brian2wolf2@gmail.com>
2022-03-21 19:53:12 -0400

    Uncommented edit submission button

--- spcarepo/verifySubmission.php
+++ spcarepo/verifySubmission.php
@@ -41,9 +41,8 @@ include_once("domain/Submission.php");
 		echo "<form action='approveSub.php' method='post'>
 		<input type='hidden' value='".$email."' name='email'>
 		<input type='submit' value='Approve Submission'></form><br>";
-		/*echo "<form action='viewNewSubs.php' method='get'>
+		echo "<form action='submissionEdit.php' method='get'>
 		<input type='submit' value='Edit Submission'></form><br><br>";
-		*/
 		echo "<form action='denySub.php' method='post'>
 		<input type='hidden' value='".$email."' name='email'>
 		<input type='submit' value='Deny Submission'></form><br>";

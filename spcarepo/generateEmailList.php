3e6ad84aba2e07ea9d00d6f3c4347e7f3ad7a27b
ssatterfield <saraidsatterfield@gmail.com>
2022-03-21 10:32:01 -0400

    more changes again

--- spcarepo/generateEmailList.php
+++ spcarepo/generateEmailList.php
@@ -3,6 +3,7 @@ session_start();
 session_cache_expire(30);
 include_once('database/dbEmails.php');
 include_once('database/dbLog.php');
+include_once('emailList.inc');
 echo('<p><strong>Email List</strong><br />');
 ?>
 <html>

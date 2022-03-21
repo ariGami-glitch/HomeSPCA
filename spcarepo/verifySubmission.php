68ecbe174b2e46370fa78c9cbdc3d327d3f92738
Jennifer Werme <jgamergurl2000@gmail.com>
2022-03-21 11:13:08 -0400

    Fixed verifySubmission page

--- spcarepo/verifySubmission.php
+++ spcarepo/verifySubmission.php
@@ -34,16 +34,16 @@ include_once("domain/Submission.php");
 		echo "<tr><td><b>Description:</b></td></tr>";
 		echo "<tr><td>".$story."</td></tr></table><br><br>";
 
-		echo "<form action='approveSub.php' method='get'>
-		<input type='hidden' value='"$email"' name='email'>
-		<input type='submit' value='Approve Submission'></form><br><br>";
+		echo "<form action='approveSub.php' method='post'>
+		<input type='hidden' value='".$email."' name='email'>
+		<input type='submit' value='Approve Submission'></form><br>";
+		//echo "<form action='viewNewSubs.php' method='get'>
+		//<input type='submit' value='Edit Submission'></form><br><br>";
+		echo "<form action='denySub.php' method='post'>
+		<input type='hidden' value='".$email."' name='email'>
+		<input type='submit' value='Deny Submission'></form><br>";
 		echo "<form action='viewNewSubs.php' method='get'>
 		<input type='submit' value='View Other Submissions'></form><br><br>";
-		echo "<form action='viewNewSubs.php' method='get'>
-		<input type='submit' value='Edit Submission'></form><br><br>";
-		echo "<form action='denySub.php' method='get'>
-		<input type='hidden' value='".$email."' name='email'>
-		<input type='submit' value='Deny Submission'></form><br><br>";
 	?>
     </body>
 </html>

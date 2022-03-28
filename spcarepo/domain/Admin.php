f289993fa731a72f80cd9ea0feb83170cbb511e0
Brian <brian2wolf2@gmail.com>
2022-03-28 18:46:25 -0400

    Created Admin.php, createAdminAccount.php, and dbAdmins.php. Fully edited Admin.php to be accurate

---
+++ spcarepo/domain/Admin.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright 2013 by Allen Tucker. 
+ * This program is part of RMHC-Homebase, which is free software.  It comes with 
+ * absolutely no warranty. You can redistribute and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation
+ * (see <http://www.gnu.org/licenses/ for more information).
+ * 
+ */
+
+/*
+ * Created on Mar 28, 2008
+ * @author Oliver Radwan <oradwan@bowdoin.edu>, Sam Roberts, Allen Tucker
+ * @version 3/28/2008, revised 7/1/2015
+ */
+ class Admin {
+	private $first_name; // first name as a string
+	private $last_name;  // last name as a string
+	private $email;   // email address as a string
+	private $username;     // username for admin account
+	private $password;     // password for admin account
+
+	function __construct($f, $l, $e, $user, $pass) {
+		$this->first_name = $f;
+		$this->last_name = $l;
+		$this->email = $e;
+		$this->username = $user;
+		$this->password = $pass;
+	}
+
+	function get_first_name() {
+		return $this->first_name;
+	}
+
+	function get_last_name() {
+		return $this->last_name;
+	}
+
+	function get_email() {
+		return $this->email;
+	}
+
+	function get_username() {
+		return $this->username;
+	}
+
+	function get_password() {
+		return $this->password;
+	}
+}
+?>

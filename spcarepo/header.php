97b440c0f665fa33c512de8ebb711434d51a3337
Ariana Tran <atran2@mail.umw.edu>
2022-03-28 11:02:38 -0400

    added the header to the viewAccSubs.php

--- spcarepo/header.php
+++ spcarepo/header.php
@@ -57,6 +57,7 @@
         $permission_array['viewSubmission.php'] = 2;
         $permission_array['log.php'] = 2;
         $permission_array['emailList.php'] = 2;
+        $permission_array['viewAccSubs.php'] = 2;
 
         //Check if they're at a valid page for their access level.
         $current_page = strtolower(substr($_SERVER['PHP_SELF'], strpos($_SERVER['PHP_SELF'],"/")+1));

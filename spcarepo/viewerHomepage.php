ec3dd36ae3744ec82525a0fe4e3412c11f90576c
Ariana Tran <atran2@mail.umw.edu>
2022-04-11 09:39:42 -0400

    hoegbefuhfhiujk

--- spcarepo/viewerHomepage.php
+++ spcarepo/viewerHomepage.php
@@ -82,6 +82,24 @@ echo( '<a href="makeNewSubmission.php"><h2>Make A Submission</h2></a>');
                 ?>
             </h1>
         </div>
+        <div class="slide">
+            <h1 class="slide-number">
+                <?php
+                //echo "hi";
+                $approved = retrieve_approved_submissions();
+                $image = $approved[2]->get_image();
+                $image_src = "pictures/".$image;
+                $name = $approved[2]->get_first_name();
+                $petname = $approved[2]->get_pet_name();
+                $petType = $approved[2]->get_pet_type();
+                echo "<table style width='400'><tr><td><img src=".$image_src." width='400' height='300'></tr></td></table>";
+                echo $name." and ". $petname. "<br>";
+                echo "Pet type: ". $petType. "<br>";
+                echo( '<a href="viewStory.php">read more</a>');
+                //echo (' <a href="viewStory.php">read more</a><br>');.*/
+                ?>
+            </h1>
+        </div>
     </div>
     </div>
 </div>

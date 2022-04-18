d4ae14f19112d093e975bda1846b2cd13e920f24
Jennifer Werme <jgamergurl2000@gmail.com>
2022-04-18 14:17:33 -0400

    added for loop and links to other pages

--- spcarepo/viewerHomepage.php
+++ spcarepo/viewerHomepage.php
@@ -1,4 +1,20 @@
-
+<style>
+.buttonlink {
+    background: none!important;
+    border: none;
+    padding: 0!important;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 19px;
+    color: #001460;  
+    cursor: pointer;
+    font-weight: bold;
+}
+.buttonLink:hover {
+    color: #3ABBAD;
+}
+</style>
 <?php
 session_start();
 session_cache_expire(30);
@@ -25,86 +41,37 @@ echo '<h1>SPCA Story Highlights</h1>';
 //echo( '<a href="login_form.php"><h2>Administrative Login</h2></a>');
 //echo('<form action="submissionEdit.php" method="get"><input type="submit" value="Make A Submission"></form>');
 echo( '<a href="login_form.php"><h2>Administrative Login</h2></a>');
-echo( '<a href="makeNewSubmission.php"><h2>Make A Submission</h2></a>');
+echo( '<a href="makeNewSubmission.php"><h2>Make A Submission</h2></a><br>');
 //echo( '<a href="viewAccSubs.php"><h2>View Approved Submissions</h2></a><br><br>');
 //echo $_SESSION['logged_in'];
 ?>
     <div id="slideshow">
-        <div class="slide-wrapper">
- 	<div class="slide">
-            <h1 class="slide-number">
-                <?php
-                $approved = retrieve_approved_submissions();
-                $image = $approved[0]->get_image();
+	<div class="slide-wrapper">
+	<?php
+	$approved = retrieve_approved_submissions();
+	for ($i = 0; $i < 4; $i++) {
+	    	echo "<div class='slide'>";
+ 		echo "<div class='slide-number'>";
+                
+                $image = $approved[$i]->get_image();
                 $image_src = "pictures/".$image;
-                $name = $approved[0]->get_first_name();
-                $petname = $approved[0]->get_pet_name();
-                $petType = $approved[0]->get_pet_type();
-                echo "<table style width='400'><tr><td><img src=".$image_src." width='400' height='300'></tr></td></table>";
-                echo $name." and ". $petname. "<br>";
-                echo "Pet type: ". $petType. "<br>";
-                echo( '<a href="viewStory.php">read more</a>');
-                //echo (' <a href="viewStory.php">read more</a><br>');.
-                ?>
-            </h1>
-        </div>
-        <div class="slide">
-            <h1 class="slide-number">
-                <?php
-                //echo "hi";
-                $approved = retrieve_approved_submissions();
-                $image = $approved[1]->get_image();
-                $image_src = "pictures/".$image;
-                $name = $approved[1]->get_first_name();
-                $petname = $approved[1]->get_pet_name();
-                $petType = $approved[1]->get_pet_type();
-                echo "<table style width='400'><tr><td><img src=".$image_src." width='400' height='300'></tr></td></table>";
-                echo $name." and ". $petname. "<br>";
-                echo "Pet type: ". $petType. "<br>";
-                echo( '<a href="viewStory.php">read more</a>');
-                //echo (' <a href="viewStory.php">read more</a><br>');.*/
-                ?>
-            </h1>
-        </div>
-        <div class="slide">
-            <h1 class="slide-number">
-                <?php
-                //echo "hi";
-                $approved = retrieve_approved_submissions();
-                $image = $approved[2]->get_image();
-                $image_src = "pictures/".$image;
-                $name = $approved[2]->get_first_name();
-                $petname = $approved[2]->get_pet_name();
-                $petType = $approved[2]->get_pet_type();
-                echo "<table style width='400'><tr><td><img src=".$image_src." width='400' height='300'></tr></td></table>";
-                echo $name." and ". $petname. "<br>";
-                echo "Pet type: ". $petType. "<br>";
-                echo( '<a href="viewStory.php">read more</a>');
-                //echo (' <a href="viewStory.php">read more</a><br>');.*/
-                ?>
-            </h1>
-        </div>
-        <div class="slide">
-            <h1 class="slide-number">
-                <?php
-                //echo "hi";
-                $approved = retrieve_approved_submissions();
-                $image = $approved[3]->get_image();
-                $image_src = "pictures/".$image;
-                $name = $approved[3]->get_first_name();
-                $petname = $approved[3]->get_pet_name();
-                $petType = $approved[3]->get_pet_type();
-                echo "<table style width='400'><tr><td><img src=".$image_src." width='400' height='300'></tr></td></table>";
-                echo $name." and ". $petname. "<br>";
-                echo "Pet type: ". $petType. "<br>";
-                echo( '<a href="viewStory.php">read more</a>');
-                //echo (' <a href="viewStory.php">read more</a><br>');.*/
-                ?>
-            </h1>
-	</div>
+                $name = $approved[$i]->get_first_name();
+                $petname = $approved[$i]->get_pet_name();
+		$petType = $approved[$i]->get_pet_type();
+		$email = $approved[$i]->get_email();
+                echo "<table style width='400'><tr><td><img src=".$image_src." width='400' height='300'></td></tr>";
+                echo "<tr><td style='text-align:center'><strong><font size='5'>" .$petname. "</font></strong>";
+		echo "<tr><td style='text-align:center'><font size='3'>Adopted by ". $name . "</font></td></tr>";
+		echo "<tr><td style='text-align:center'><form method='POST' action='viewStory.php'>
+		<input type='hidden' name='email' value='".$email."'>
+		<input type='submit' class='buttonlink' value='read more'></form></td></tr></table>";
+                
+		echo "</div></div>";
+	}
+        
+	?>
     </div>
     </div> 
-	<?php //include('footer2.inc'); ?>
 </div></div></div>
 	<?php include('footer2.inc'); ?>
 </body>
